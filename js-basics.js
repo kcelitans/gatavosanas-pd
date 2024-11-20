9b0ce171ff61e924f83e94ae8bc62b74c99bee2c
Kristofers <you@example.com>
2024-11-20 15:39:13 +0200

    first commit

---
+++ js-basics.js
@@ -0,0 +1,65 @@
+
+// // variables
+// // let firstName = "KristofersC"
+
+// // console.log(firstName);
+
+
+
+// // let myArray = [1,2,3,4]
+
+// // // conditionals
+// // let x = 11;
+// // if (x == 10){
+// //     console.log("X ir lielāks par 10")
+// // }
+
+// let person = {
+//     vārds: "Antra", 
+//     irStudents: true 
+// }
+// console.log(person.irStundents);  //dot notation
+// console.log(person["vārds"]);  //bracket notation
+
+// let attribute = "vārds"
+// console.log(person[attribute])
+
+// console.log(person)
+// person.vecums = 16
+// console.log(person)
+
+// // ternary operator
+// let ageStatus = (person.vecums >= 18) ? "Pilngadīgs cilvēks" : "Nepilngadīgs"
+// console.log(ageStatus)
+
+// let namesArray = ["Antra", "Ilmārs", "Renārs", "Pauls"]       //indexi  ir attiecīgi [0, 1, 2, 3]
+// namesArray[2]
+
+
+// for (let i = 0; i < namesArray.lenght; i++){
+//     (console.log[i])
+// }
+
+// functions with and without return
+// function drive(){
+//     console.log("Driving")
+// }
+
+
+// function getRectArea() {
+//     return 3 * 4
+// }
+// let result = getRectArea()
+// console.log(result)
+
+const getRectAreaAdvanced = function (width, height) {
+    return width * height;
+  };
+  
+  console.log(getRectAreaAdvanced(3, 4));
+  
+  
+
+
+
+
